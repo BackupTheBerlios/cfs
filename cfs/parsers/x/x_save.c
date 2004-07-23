604a8ae7593b9fdcafa870318e58ced18f4464d5
parazoid <parazoid>
2004-07-23 15:03:16 +0000

    X configs now save correctly

--- cfs/parsers/x/x_save.c
+++ cfs/parsers/x/x_save.c
@@ -20,12 +20,19 @@ static int x_save_recurs(FILE * f, cfile root, int level){
 		fprintf(f,"%s",nullsafe(cur->value));
 	    else
 	    	fprintf(f,"%s %s\n",cur->name,nullsafe(cur->value));
-	} else	
-	    fprintf(f,"<%s %s>\n",cur->name,nullsafe(cur->value));
+	} else {	
+	    if(level==0)
+		fprintf(f,"Section %s\n",cur->name);
+	    else
+		fprintf(f,"SubSection %s\n",cur->name);
+	}
 	
 	if(cur->first_child){
 	    x_save_recurs(f,cur,level+1);
-	    fprintf(f,"</%s>\n",cur->name);
+	    if(level==0)
+	        fprintf(f,"EndSection\n");
+	    else
+		fprintf(f,"EndSubSection\n");
 	}
 	cur = cur->next;
     }
